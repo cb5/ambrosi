f309518756b411d48cee55c917a192a89d04fc28
Your Name <you@example.com>
2016-04-08 16:16:54 +0200

    Afegir ambrosio.py

--- ambrosio/ambrosio.py
+++ ambrosio/ambrosio.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from commandlist import CommandList
+import channels as ch
+import actions as ac
+import time
 
 class Ambrosio(object):
     """Class for Ambrosio Personal Digital Butler
 
-  will run our house"""
-  def __init__(self):
-      super(Ambrosio,self).__init__()
+    Will run our house"""
+    def __init__(self):
+        super(Ambrosio, self).__init__()
+        self.cl = CommandList()
+        self.channels = []
+        self.channels.append(ch.TextChannel())
 
+        self.actions =[]
+        self.actions.append(ac.MusicPlayer())
 
-  def mainloop(self):
-      # while True:
-      # command = get_command
-      # do_command(command)
-      # update
-      pass
+    def next_command(self):
+        try:
+            return self.cl.next()
+        except:
+            return None
 
-if __name__=="__main__":
+    def update_channels(self):
+        for chan in self.channels:
+            while chan.msg_avail():
+                self.cl.append(chan.get_msg())
+
+    def execute_command(self,command):
+        print "Will execute", command
+        # Foreach Action in actions
+        # if is_for_you
+        # action.do
+        words = command.split()
+        first_words = words[0]
+        rest_words = words[1:]
+        for ac in self.actions:
+            if ac.is_for_you(first_words):
+                ac.do(rest_words)
+                break
+            else:
+                print "No t'entenc"
+
+    def mainloop(self):
+        # While True:
+        #   command = get_command
+        #   do_command(command)
+        #   update
+        while True:
+            command = self.next_command()
+            if command:
+                self.execute_command(command)
+            time.sleep(1)
+            self.update_channels()
+
+if __name__ == "__main__":
     print "Here be dragons!"
+    amb = Ambrosio()
+    amb.mainloop()
